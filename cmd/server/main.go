3c5d82b563cd33c2f25f315c5c560333f4124c4d
MPichead <pichead.m@perfectcomsolutions.com>
2024-11-27 15:46:42 +0700

    fix vercel json for deploy vercel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,18 +16,34 @@ var books []Book
 
 func main() {
 
-	app := fiber.New()
+	isProd := false
 
-	books = append(books, Book{ID: 1, Title: "HTML 101", Author: "Pichead"})
-	books = append(books, Book{ID: 2, Title: "CSS 101", Author: "Maneerat"})
+	app := fiber.New(fiber.Config{
+		Prefork:       isProd,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Fiber",
+		AppName:       "Go LLM Chat App v0.0.4",
+	})
+
+	app.Static("/", "./public")
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString(("hello world"))
 	})
 
-	app.Get("/books", getBooks)
-	app.Get("/books/:id", getBook)
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+
+	v1.Get("/books", getBooks)
+	v1.Get("/books/:id", getBook)
+	v1.Post("/books", createBook)
+	v1.Put("/books/:id", updateBook)
 
 	app.Listen(":8000")
+
+	books = append(books, Book{ID: 1, Title: "HTML 101", Author: "Pichead"})
+	books = append(books, Book{ID: 2, Title: "CSS 101", Author: "Maneerat"})
 
 }
 
@@ -47,5 +63,40 @@ func getBook(c *fiber.Ctx) error {
 		}
 	}
 
-	return c.JSON(books)
+	return c.SendStatus(fiber.StatusNotFound)
+}
+
+func createBook(c *fiber.Ctx) error {
+	book := new(Book)
+
+	if err := c.BodyParser(book); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	books = append(books, *book)
+	return c.JSON(c.Request().Body())
+}
+
+func updateBook(c *fiber.Ctx) error {
+	id, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	bookUpdate := new(Book)
+
+	if err := c.BodyParser(bookUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for i, book := range books {
+		if book.ID == id {
+			books[i].Author = bookUpdate.Author
+			books[i].Title = bookUpdate.Title
+			return c.JSON(books[i])
+		}
+	}
+
+	return c.SendStatus(fiber.StatusNotFound)
 }
