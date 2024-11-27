04f422b51bc0acd2fb1402f3e6992225b234a562
MPichead <pichead.m@perfectcomsolutions.com>
2024-11-27 17:18:40 +0700

    fix : change fiber app from internal to pkg

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"log"
 
-	"github.com/pichead/go-llm-chat/internal/app"
+	"github.com/pichead/go-llm-chat/pkg/app"
 )
 
 func main() {
 
 	app := app.FiberApp()
 
-	// รันเซิร์ฟเวอร์
 	log.Fatal(app.Listen(":8000"))
 
 }
